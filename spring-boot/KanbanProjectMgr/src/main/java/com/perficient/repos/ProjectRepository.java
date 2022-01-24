e4bbae09967c7d7ebd64525cbbbe7ffeec71a64d
Rinkart Asibey <rinkart.asibey@perficient.com>
2022-01-23 20:37:56 -0600

    no comment

---
+++ spring-boot/KanbanProjectMgr/src/main/java/com/perficient/repos/ProjectRepository.java
@@ -0,0 +1,15 @@
+package com.perficient.repos;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.perficient.entities.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long>{
+
+	@Override
+	Iterable<Project> findAllById(Iterable<Long> ids);
+		
+
+}

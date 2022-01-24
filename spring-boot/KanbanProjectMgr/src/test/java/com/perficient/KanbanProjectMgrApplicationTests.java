82e037904fd4ecbeec5fe93300f7bac847c1ce58
Rinkart Asibey <rinkart.asibey@perficient.com>
2022-01-23 18:39:42 -0600

    first commit

---
+++ spring-boot/KanbanProjectMgr/src/test/java/com/perficient/KanbanProjectMgrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perficient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanProjectMgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

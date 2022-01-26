d6ddc3366dc4c37dd9a9b6d55567fd847e56a0b3
Rinkart Asibey <rinkart.asibey@perficient.com>
2022-01-25 18:22:43 -0600

    text field created

--- frontend/my-ui/src/App.tsx
+++ frontend/my-ui/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import InputField from './components/InputField';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      Getting started
+      <span className="heading">Taskify</span>
+      <InputField/>
     </div>
   );
 }
---
+++ frontend/my-ui/src/components/InputField.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function InputField() {
+  return <form>
+      <input type="input" placeholder='enter a value' className='input__box'></input>
+  </form>;
+}
+
+export default InputField;
